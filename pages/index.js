ed89289737566bc708e47e99d2dc8a4cbc6e2f2f
Guilherme Segabinassi Da Cunha <112842374+GuiSegabinassi@users.noreply.github.com>
2024-11-24 17:10:30 +0000

    Adiciona scripts

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,19 @@
 function Home() {
-    return <h1>Eu estou trabalhando em um projeto e precisava testar uma url, ai lembrei de voce, então quando vc clicar no link, pq eu vou te mandar kkk saiba que sim, eu faço questão de te ver nem que seja por um minuto! sz</h1>
+  return (
+    <h1>
+      Eu estou trabalhando em um projeto e precisava testar uma url, ai lembrei
+      de voce, então quando vc clicar no link, pq eu vou te mandar kkk saiba que
+      sim, eu faço questão de te ver nem que seja por um minuto! sz
+    </h1>
+  );
+}
+
+function teste() {
+  alert("olá, mundo!");
+}
+
+function teste2() {
+  alert("erro na identação");
 }
 
 export default Home;
