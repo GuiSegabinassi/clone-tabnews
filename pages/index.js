32774bc3de2cc9bf9951fb1fb8e31f42f9aac778
Guilherme Segabinassi Da Cunha <112842374+GuiSegabinassi@users.noreply.github.com>
2024-11-28 02:45:10 +0000

    mudando a mensagem da pagina index, em construção

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,5 @@
 function Home() {
-  return (
-    <h1>
-      Eu estou trabalhando em um projeto e precisava testar uma url, ai lembrei
-      de voce, então quando vc clicar no link, pq eu vou te mandar kkk saiba que
-      sim, eu faço questão de te ver nem que seja por um minuto! sz
-    </h1>
-  );
+  return <p>Olá, Mundo!</p>;
 }
 
 export default Home;
